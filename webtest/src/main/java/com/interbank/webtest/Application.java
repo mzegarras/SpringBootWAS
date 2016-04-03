5408caf8db704895727dc1a31672f45a22757e3f
manuelzegarra <mzegarra@gmail.com>
2016-04-02 19:52:12 -0500

    Soporte External config

--- webtest/src/main/java/com/interbank/webtest/Application.java
+++ webtest/src/main/java/com/interbank/webtest/Application.java
@@ -1,5 +1,7 @@
 package com.interbank.webtest;
 
+import java.util.Properties;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -7,6 +9,7 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 //import org.springframework.integration.jmx.config.EnableIntegrationMBeanExport;
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -19,12 +22,12 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @EnableAutoConfiguration
 //@ComponentScan({"net.msonic.pos.ws.*"})
 @EnableConfigurationProperties
-
+@PropertySource(value = { "file:/Proyectos/Apps/config/application.yml" })
 public class Application extends SpringBootServletInitializer  implements WebApplicationInitializer{
 		
 	
 	
-	 public static void main(String[] args) {
+	 	public static void main(String[] args) {
 	        SpringApplication.run(applicationClass, args);
 	    }
 
@@ -34,6 +37,43 @@ public class Application extends SpringBootServletInitializer  implements WebApp
 	    }
 
 	    private static Class<Application> applicationClass = Application.class;
+	
+	
+	/*
+	@Override
+	 protected SpringApplicationBuilder configure(SpringApplicationBuilder springApplicationBuilder) {
+	    return springApplicationBuilder
+	            .sources(Application.class)
+	            .properties(getProperties());
+	 }
+
+	 public static void main(String[] args) {
+
+	    SpringApplicationBuilder springApplicationBuilder = new SpringApplicationBuilder(Application.class);
+	    
+	    springApplicationBuilder.sources(Application.class)
+        						.properties(getProperties())
+        						.run(args);
+	            
+	 }
+	
+	    
+	    static Properties getProperties() {
+	        Properties props = new Properties();
+	        //props.put("spring.config.location", "classpath:myapp1");
+	        
+	        ├src
+			| └main
+			|   └resources
+			|     └myapp1
+			|       └application.properties
+			|       └application-development.properties
+			|       └logback.xml
+			└─pom.xml
+	         
+	        props.put("spring.config.location", "/Proyectos/Apps/config");
+	        return props;
+	      }*/
 }
 
 
